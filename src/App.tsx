581aeefdfa6f7cd84d1c1c48f7f5ca376d84d2af
srimobiles <sriammansmartstore@gmail.com>
2025-12-11 16:10:32 +0530

    fully changed

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ import RetreatAudience from './pages/RetreatAudience';
 import RetreatPricing from './pages/RetreatPricing';
 import './pages/RetreatBase.css'
 import './pages/RetreatAnimations.css'
-
-import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 
 const RetreatPage: React.FC = () => {
   React.useEffect(() => {
@@ -47,138 +47,78 @@ const RetreatPage: React.FC = () => {
       
 
       {/* 1. HERO - Strong USP, Clear Value Proposition */}
-      <div data-aos="fade-zoom-in" data-aos-duration="400">
-        <RetreatHeroLanding />
-      </div>
+      <RetreatHeroLanding />
 
       {/* 1.5. SPARK PRESENTATION - Brand Introduction */}
-      <div data-aos="fade-up" data-aos-duration="400" data-aos-delay="50">
-        <SparkPresentation />
-      </div>
+      <SparkPresentation />
 
       {/* 2. WHO IS THIS FOR - Clear Positioning */}
-      <div data-aos="fade-left" data-aos-duration="400" data-aos-delay="50">
-        <RetreatAudience />
-      </div>
-
-
-
+      <RetreatAudience />
 
       {/* 3. MANJIRI PRABHU - Special Section */}
-      <div data-aos="flip-left" data-aos-duration="400" data-aos-delay="80">
-        <ManjiriPrabhu />
-      </div>
-
-
+      <ManjiriPrabhu />
 
       {/* 4. SANGEETHA BAHADUR - Special Section */}
-      <div data-aos="flip-right" data-aos-duration="400" data-aos-delay="100">
-        <SangeethaBahadur />
-      </div>
-
-   
+      <SangeethaBahadur />
 
       {/* 4. FACILITATOR - Credibility & Trust (Captain Sahana) */}
-      <div data-aos="fade-right" data-aos-duration="400" data-aos-delay="120">
-        <RetreatFacilitator />
-      </div>
+      <RetreatFacilitator />
 
-
-   {/* 5. SUNIL SHELAR - Special Section */}
-      <div data-aos="fade-left" data-aos-duration="400" data-aos-delay="140">
-        <SunilShelar />
-      </div>
-
-
-    
+      {/* 5. SUNIL SHELAR - Special Section */}
+      <SunilShelar />
 
       {/* 5. OVERVIEW - What You'll Experience */}
-      <div data-aos="zoom-in" data-aos-duration="400" data-aos-delay="80">
-        <RetreatHeroTwoColumn />
-      </div>
-
+      <RetreatHeroTwoColumn />
 
       {/* 6. KEY TAKEAWAYS & ITINERARY */}
-      <div className="theme-amber" data-aos="fade-up-right" data-aos-duration="400" data-aos-delay="100">
+      <div className="theme-amber">
         <RetreatItinerary />
       </div>
 
-
       {/* 7. DAILY EXPERIENCE */}
-      <div className="theme-lime" data-aos="fade-up-left" data-aos-duration="400" data-aos-delay="120">
+      <div className="theme-lime">
         <RetreatDaily />
       </div>
 
-
       {/* 8. THE LOCATION - Visual Storytelling of Himalayas */}
-      <div className="theme-sand" data-aos="zoom-in-up" data-aos-duration="400" data-aos-delay="100">
+      <div className="theme-sand">
         <RetreatVilla />
       </div>
 
-
       {/* 9. STAY & RESORT */}
-      <div data-aos="fade-down" data-aos-duration="400" data-aos-delay="80">
-        <RetreatStay />
-      </div>
-
+      <RetreatStay />
 
       {/* 10. ACTIVITIES & SIGHTSEEING */}
-      <div data-aos="fade-up" data-aos-duration="400" data-aos-delay="100">
-        <RetreatActivities />
-      </div>
-
+      <RetreatActivities />
 
       {/* 11. CUISINE */}
-      <div data-aos="fade-right" data-aos-duration="400" data-aos-delay="120">
-        <RetreatCuisine />
-      </div>
-
+      <RetreatCuisine />
 
       {/* 12. TESTIMONIALS - Social Proof */}
-      <div data-aos="fade-left" data-aos-duration="400" data-aos-delay="100">
-        <RetreatTestimonials />
-      </div>
-
+      <RetreatTestimonials />
 
       {/* 13. PRICING & INCLUSIONS - Clear Value */}
-      <div id="pricing" data-aos="zoom-in-down" data-aos-duration="400" data-aos-delay="100">
+      <div id="pricing">
         <RetreatPricing />
       </div>
 
-
       {/* 14. WHY THIS RETREAT */}
-      <div data-aos="fade-up" data-aos-duration="400" data-aos-delay="100">
-        <RetreatWhy />
-      </div>
-
+      <RetreatWhy />
 
       {/* 15. TRANSPORT & LOGISTICS */}
-      <div data-aos="fade-down" data-aos-duration="400" data-aos-delay="80">
-        <TransportArrangements />
-      </div>
-
+      <TransportArrangements />
 
       {/* 16. ESSENTIALS - What to Bring */}
-      <div data-aos="fade-right" data-aos-duration="400" data-aos-delay="120">
-        <RetreatEssentials />
-      </div>
-
+      <RetreatEssentials />
 
       {/* 17. WRITING PLAN SUPPORT */}
-      <div data-aos="fade-left" data-aos-duration="400" data-aos-delay="100">
-        <BookWritingPlan />
-      </div>
-
+      <BookWritingPlan />
 
       {/* 18. FAQ - Address All Concerns */}
-      <div id="faq" data-aos="zoom-in" data-aos-duration="400" data-aos-delay="100">
+      <div id="faq">
         <RetreatFAQ />
       </div>
 
-
-      
-
- 
       <WhatsappFloat />
     </div>
   )
